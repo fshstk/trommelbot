8d603dd10883efccba3263cba8c612efb7560571
fshstk <fshstk@fshstk.com>
2020-11-08 00:29:30 +0100

    Use message embed instead of primitive message for info text

--- commands.js
+++ commands.js
@@ -1,10 +1,4 @@
-const infoText = `
-============================================================
-Der digitale Trommelkreis ist eine regelmäßige Veranstaltung.
-============================================================
-`;
-
-/* ========================================================================== */
+const { MessageEmbed } = require("discord.js");
 
 const { global } = require("./global");
 const {
@@ -17,7 +11,33 @@ const { playURL } = require("./voice_channel");
 const regularCommands = {
     trommel: (msg) => msg.channel.send("...kreis!"),
     info: (msg) => {
-        msg.reply(infoText);
+        const reply = new MessageEmbed()
+            .setColor("#FF0000")
+            .setTitle("Der offizielle Discord Bot des digitalen Trommelkreises")
+            .setAuthor("Trommelbot", "https://www.trommelkreis.club/static/img/logo.png")
+            .setDescription(`
+            ‣ Der Trommelkreis findet *jeden zweiten Montag um 19:30 Uhr* statt.
+            ‣ Gemeinsames Anhören findet hier um ca. 22:00/22:30 Uhr statt.
+            ‣ Mehr zum digitalen Trommelkreis erfährt ihr auf der [Webseite](https://trommelkreis.club)!
+            ‣ Wenn ihr mitmachen wollt, lohnt es sich der [WhatsApp Gruppe](https://chat.whatsapp.com/IuA760mHIrcKiR3krPjBwK) beizutreten.
+            ‣ Jeder ist willkommen!
+
+            **Verfügbare Commands:** (TrommelBot)
+            ‣ \`${global().prefix}info\`: diese Nachricht anzeigen
+            ‣ \`${global().prefix}sesh load\`: lade die heutige Session.
+            ‣ \`${global().prefix}sesh load [YYYYMMDD]\`: lade die Session des angegebenen Datums.
+            ‣ \`${global().prefix}sesh load [URL]\`: lade die Session der angegebenen URL.
+            ‣ \`${global().prefix}sesh info\`: Informationen zur aktuell geladenen Session
+            ‣ \`${global().prefix}sesh next\`: Spiele das nächste Lied der aktuell geladenen Session.
+            ‣ \`${global().prefix}play/pause\`: Play/Pause
+            ‣ \`${global().prefix}stop\`: Stopp, vergesse die aktuell geladene Session.
+
+            **Verfügbare Commands**: ([MEE6](https://mee6.xyz/))
+            ‣ \`!help\`: Hilfetext anzeigen
+            ‣ \`!play [URL]\`: Link abspielen (YouTube / SoundCloud / MP3)
+            ‣ \`!pause/stop\`: Pause / Stopp
+            `);
+        return msg.channel.send(reply);
     },
 };
 
