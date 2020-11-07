c632f701b0db59d168ff475847f0418a02cf058c
fshstk <fshstk@fshstk.com>
2020-11-08 00:48:35 +0100

    Clean up init code a little. Also, always output debug code since nobody's gonna see it anyway and it's always useful

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const Discord = require("discord.js");
 const { commandPrefix } = require("./config.json");
 const { global } = require("./global");
 
-const {
-    BOT_TOKEN, ADMIN_ID, CHANNEL_ID, DEBUG,
-} = process.env;
-
+const { BOT_TOKEN, ADMIN_ID, CHANNEL_ID } = process.env;
 
 global().bot = new Discord.Client();
 global().locked = true;
@@ -15,17 +12,19 @@ global().prefix = commandPrefix;
 require("./commands");
 const { confirmVoiceChannelSetup } = require("./voice_channel");
 
-global().bot.on("ready", async () => {
-    console.log(`Logged in as ${global().bot.user.tag}!`);
-    global().voiceChannel = await global().bot.channels.fetch(CHANNEL_ID);
-    global().adminUser = await global().bot.users.fetch(ADMIN_ID);
-    confirmVoiceChannelSetup();
-});
+global().bot
+    .on("error", console.error)
+    .on("warn", console.warn)
+    .on("debug", console.log)
+    .on("ready", async () => {
+        console.log(`Logged in as ${global().bot.user.tag}!`);
+        global().voiceChannel = await global().bot.channels.fetch(CHANNEL_ID);
+        global().adminUser = await global().bot.users.fetch(ADMIN_ID);
+        confirmVoiceChannelSetup();
+        global().bot.user.setActivity("message", { type: "CUSTOM_STATUS" }); // not sure if works
+    });
 
 // const loadSession = () => {
 //     // load session from REST API
 // };
-global().bot.on("debug", (msg) => {
-    if (DEBUG === "1") console.log(msg);
-});
 global().bot.login(BOT_TOKEN);
